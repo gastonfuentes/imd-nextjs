ee75d3406568398de2ef63e10e9779e1d4167696
gastonfuentes <gastonnicolasfuentes@gmail.com>
2025-03-26 12:11:04 -0300

    se componetizo la card inmueble

--- app/inmuebles/index.ts
+++ app/inmuebles/index.ts
@@ -1,2 +1,5 @@
+export { InmueblesGrid } from './components/InmueblesGrid'
+export { InmuebleCard } from './components/InmuebleCard'
+
 export type { InmueblesResponse } from './interfaces/inmuebles-response';
 export type { SimpleInmueble } from './interfaces/simple-inmueble';
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Banner } from "@/components/Banner";
 import { fetchProperties } from '../lib/fetch-properties';
-import Image from "next/image";
+import { InmueblesGrid } from "./inmuebles";
 
 
 export default async function Home() {
@@ -11,40 +11,7 @@ export default async function Home() {
     <div className="">
       <main className="">
         <Banner />
-        {inmuebles.map((inmueble) => (
-          <div key={inmueble.id} className=" flex flex-col items-center justify-center gap-4 border-2 border-black p-4 ">
-            <h2>{inmueble.title}</h2>
-            <div className="flex gap-2">
-              {inmueble.images.map((image, index) => (
-                /* <img
-                  key={index}
-                  src={image}
-                  alt={`Imagen ${index + 1} de ${inmueble.title}`}
-                  className="w-32 h-32 object-cover"
-                /> */
-                <Image
-                  src={image}
-                  alt={`Imagen ${index + 1} de ${inmueble.title}`}
-                  width={150}
-                  height={150}
-                  key={index}
-                />
-              ))}
-            </div>
-            <div>
-              <p>{inmueble.descripcion}</p>
-              <p>{inmueble.direccion}</p>
-              <p>{inmueble.precio}</p>
-              <p>{inmueble.superficie_construida_total}</p>
-              <p>{inmueble.superficie_del_terreno}</p>
-              <p>{inmueble.superficie_cubierta_total}</p>
-              <p>{inmueble.quincho}</p>
-              <p>{inmueble.dormitorios}</p>
-              <p>{inmueble.cochera}</p>
-              <p>{inmueble.plantas}</p>
-            </div>
-          </div>
-        ))}
+        <InmueblesGrid inmuebles={inmuebles} />
       </main>
     </div>
   );
