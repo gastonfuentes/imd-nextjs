ed193d6eff79f9184889cf4ab7ea988b8a781ac7
gastonfuentes <gastonnicolasfuentes@gmail.com>
2025-04-01 22:10:59 -0300

    se comenzo a configurar el search

--- components/SearchImd/SearchImd.tsx
+++ components/SearchImd/SearchImd.tsx
@@ -5,10 +5,14 @@ import { SearchTypeProperty } from "../SearchTypeProperty";
 
 export function SearchImd() {
     return (
-        <div className="flex-col justify-between gap-4 py-4 bg-gray-500 rounded-md flex md:flex-row backdrop-blur-lg shadow-accent mt-8">
-            <SearchLocation />
-            <SearchTypeProperty />
-            <SearchTypeOperation />
+        <div className="flex-col justify-between gap-4 p-8 bg-gray-500 rounded-md flex backdrop-blur-lg shadow-accent mt-8">
+            <div>
+                <SearchTypeOperation />
+            </div>
+            <div className="flex flex-row justify-between gap-4">
+                <SearchLocation />
+                <SearchTypeProperty />
+            </div>
         </div>
     )
 }
--- components/SearchTypeOperation/SearchTypeOperation.tsx
+++ components/SearchTypeOperation/SearchTypeOperation.tsx
@@ -1,5 +1,12 @@
+import { Tabs, TabsList, TabsTrigger } from "../ui/tabs";
+
 export function SearchTypeOperation() {
     return (
-        <p>Search location</p>
+        <Tabs defaultValue="account" className="w-[400px]">
+            <TabsList className="grid w-full grid-cols-2">
+                <TabsTrigger value="account">Alquilar</TabsTrigger>
+                <TabsTrigger value="password">Comprar</TabsTrigger>
+            </TabsList>
+        </Tabs>
     )
 }
