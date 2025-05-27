7344da824b019bb6700111d4626a84c1fe841bce
gastonfuentes <gastonnicolasfuentes@gmail.com>
2025-05-26 21:19:05 -0300

    se puso revalidate a al fetch q busca las ciudades

--- lib/fetch-ubicaciones.ts
+++ lib/fetch-ubicaciones.ts
@@ -3,7 +3,7 @@ import { UbicacionesResponse } from '@/inmuebles/interfaces/ubicaciones-response
 
 
 export const fetchUbicaciones = async (): Promise<SimpleUbicacion[]> => {
-    const res: UbicacionesResponse[] = await fetch("https://bisque-giraffe-421578.hostingersite.com/wp-json/wp/v2/ciudades")
+    const res: UbicacionesResponse[] = await fetch("https://bisque-giraffe-421578.hostingersite.com/wp-json/wp/v2/ciudades", { next: { revalidate: 60 } })
         .then((res) => res.json());
 
     const ubicaciones = res.map((tipo) => ({
